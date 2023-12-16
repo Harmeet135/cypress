a05593ab521455e108526e24e6847556baa6c447
Harmeet Singh <harmeetchahal9891@gmail.com>
2023-12-16 20:39:34 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import db from '@/lib/supabase/db'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +16,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  console.log('db');
+  console.log(db);
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
---
+++ src/lib/supabase/schema.ts
@@ -0,0 +1,48 @@
+import { pgTable , uuid  ,timestamp , text} from 'drizzle-orm/pg-core';
+
+export const workspaces = pgTable( 'workspaces' , {
+    id : uuid('id').defaultRandom().primaryKey().notNull(),
+    createdAt: timestamp('created_at' , {
+        withTimezone: true,
+        mode: 'string',
+    }),
+    workspaaceOwner: uuid('workspace_owner').notNull(),
+    title : text('title').notNull(),
+    iconId: text('icon_id').notNull(),
+    data: text('data'),
+    inTrash: text('in_trash'),
+    bannerUrl: text('banner_url'),
+});
+
+
+export const folders = pgTable( 'folders' , {
+    id : uuid('id').defaultRandom().primaryKey().notNull(),
+    createdAt: timestamp('created_at' , {
+        withTimezone: true,
+        mode: 'string',
+    }),
+    workspaaceOwner: uuid('workspace_owner').notNull(),
+    title : text('title').notNull(),
+    iconId: text('icon_id').notNull(),
+    data: text('data'),
+    inTrash: text('in_trash'),
+    bannerUrl: text('banner_url'),
+    workspaceId: uuid('workspace_id').references(() =>  workspaces.id , {onDelete: 'cascade'}),
+});
+
+
+export const files  = pgTable( 'files' , {
+    id : uuid('id').defaultRandom().primaryKey().notNull(),
+    createdAt: timestamp('created_at' , {
+        withTimezone: true,
+        mode: 'string',
+    }),
+    workspaaceOwner: uuid('workspace_owner').notNull(),
+    title : text('title').notNull(),
+    iconId: text('icon_id').notNull(),
+    data: text('data'),
+    inTrash: text('in_trash'),
+    bannerUrl: text('banner_url'),
+    workspaceId: uuid('workspace_id').references(() =>  workspaces.id , {onDelete: 'cascade'}).notNull(),
+    folderid: uuid('folder_id').references(() =>  folders.id , {onDelete: 'cascade'}),
+});
